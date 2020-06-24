ee245c4e719dfc58526da2040ddb68d8f5866960
parkers4 <67388078+parkers4@users.noreply.github.com>
2020-06-24 22:51:28 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -104,7 +104,6 @@ $(document).ready(function() {
     $('#home').backstretch([
        "images/home-bg-slideshow1.jpg", 
        "images/home-bg-slideshow2.jpg",
-       "images/home-bg-slideshow3.jpg",
         ],  {duration: 2000, fade: 750});
     });
   })
